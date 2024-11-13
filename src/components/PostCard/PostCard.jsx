18ad778e7c87a11bb813f77954eefd846bbc4972
Gargenziano03 <gargenziano03@gmail.com>
2024-11-13 18:45:25 +0100

    add data.published

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,12 +1,15 @@
 export default function PostCard({ data }) {
     return (
-        <div className="post_card">
-            <h2>{data.title}</h2>
-            <img src={data.image} alt="" />
-            <div className="details">
-                <p>{data.content}</p>
-                <span>{data.tags}</span>
+        data.published && (
+            <div className="post_card">
+                <h2>{data.title}</h2>
+                <img src={data.image} alt="" />
+                <div className="details">
+                    <p>{data.content}</p>
+                    <span>{data.tags}</span>
+
+
+                </div>
             </div>
-        </div>
-    )
+        ))
 }
